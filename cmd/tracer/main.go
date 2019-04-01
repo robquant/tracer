63c6c75f063b4129ee367a255e6edef88835ab07
Robert Franke <franke.rob@gmail.com>
2019-04-01 15:20:13 +0200

    Visualize surface normals using color

--- cmd/tracer/main.go
+++ cmd/tracer/main.go
@@ -7,24 +7,32 @@ import (
 	"github.com/robquant/tracer/pkg"
 )
 
-func color(r *pkg.Ray) pkg.Color {
-	if hitSphere(pkg.NewVec3(0, 0, -11), 0.5, r) {
-		return pkg.Red
-	}
-	unitDirection := r.Dir().Normed()
-	t := 0.5 * (unitDirection.Y() + 1.0)
-	c1 := pkg.NewVec3(1.0, 1.0, 1.0).Mul(1.0 - t)
-	c2 := pkg.NewVec3(0.5, 0.7, 1.0).Mul(t)
-	return pkg.Color{Vec3: c1.Add(c2)}
-}
-
-func hitSphere(center pkg.Vec3, radius float64, r *pkg.Ray) bool {
+func hitSphere(center pkg.Vec3, radius float64, r *pkg.Ray) float64 {
 	oc := r.Orig().Sub(center)
 	a := r.Dir().Dot(r.Dir())
 	b := 2.0 * r.Dir().Dot(oc)
 	c := oc.Dot(oc) - radius*radius
 	discriminant := b*b - 4*a*c
-	return discriminant > 0
+	if discriminant < 0 {
+		return -1.0
+	} else {
+		return (-b - math.Sqrt(discriminant)) / (2.0 * a)
+	}
+}
+
+func color(r *pkg.Ray) pkg.Color {
+	negZ := pkg.NewVec3(0, 0, -1)
+	t := hitSphere(negZ, 0.5, r)
+	if t > 0 {
+		N := r.At(t).Sub(negZ)
+		(&N).Normalize()
+		return pkg.NewColor((N.X()+1)*0.5, (N.Y()+1)*0.5, (N.Z()+1)*0.5)
+	}
+	unitDirection := r.Dir().Normed()
+	t = 0.5 * (unitDirection.Y() + 1.0)
+	c1 := pkg.NewVec3(1.0, 1.0, 1.0).Mul(1.0 - t)
+	c2 := pkg.NewVec3(0.5, 0.7, 1.0).Mul(t)
+	return pkg.Color{Vec3: c1.Add(c2)}
 }
 
 func main() {
